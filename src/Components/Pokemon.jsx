39d2eb5e2296fac2f62833380cc16f19a0d5eb0a
MosDevx <mosesk.wamae@gmail.com>
2023-01-14 13:12:38 +0300

    add query for Pokemon List

--- src/Components/Pokemon.jsx
+++ src/Components/Pokemon.jsx
@@ -1,27 +1,33 @@
-
-import React from 'react'
-import { useGetPokemonByNameQuery } from '../redux/pokemonApi'
+import React from 'react';
+import { useGetPokemonByNameQuery, useGetListOfPokemonQuery } from '../redux/pokemonApi';
 
 const Pokemon = () => {
-	const {data,error,isLoading} = useGetPokemonByNameQuery('bulbasaur')
-	return (
-		<div>
-			{
+  // const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
+  const { data,error,isLoading } = useGetListOfPokemonQuery();
+	console.log(data?.results[0].name)
+  return (
+    <div>
+      {/* {
 				error ? (
-					<p>Woopsie, an error occured</p>
+  <p>Woopsie, an error occured</p>
 				) : isLoading ? (
-					<p>Loading Hold your horses</p>
-				) : data ?(
-					<>
-						<h3>{data.name}</h3>
-						<img src={data.image} alt={data.name} />
+  <p>Loading Hold your horses</p>
+				) : data ? (
+  <>
+    <h3>{data.name}</h3>
+    <img className="w-48 h-48" src={data.image} alt={data.name} />
 
-					</>
+  </>
 				) : null
-			}
+			} */}
 
-		</div>
-	)
-}
+      <div>
+        List of pokemon
+		
+        {data ? (data.results.map((pokemon) => <div key={pokemon.name}>{pokemon.name}</div>)) : ("Nothing")}
+      </div>
+    </div>
+  );
+};
 
-export default Pokemon
+export default Pokemon;
--- src/redux/pokemonApi.js
+++ src/redux/pokemonApi.js
@@ -1,26 +1,37 @@
-import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const pokemonApi = createApi({
-	reducerPath:'pokemonApi',
-	baseQuery:fetchBaseQuery({ baseUrl:'https://pokeapi.co/api/v2/'}),
-	endpoints:(builder)=>({
-		getPokemonByName:builder.query({
-			query:(name)=>`pokemon/${name}`,
-			transformResponse:(response,meta,arg)=>{
-				console.log(response)
-			let {abilities, base_experience:xp, height,weight,sprites:{front_shiny:image},types} = response
-				return {
-					abilities,
-					xp,
-					height,
-					weight,
-					image,
-					types
-				}
-			}
-		})
-	}),
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+  endpoints: (builder) => ({
+    getPokemonByName: builder.query({
+      query: (name) => `pokemon/${name}`,
+      transformResponse: (response, meta, arg) => {
+        console.log(response);
+        const {
+          abilities, base_experience: xp, height, weight, sprites: { front_shiny: image }, types,
+        } = response;
+        return {
+          abilities,
+          xp,
+          height,
+          weight,
+          image,
+          types,
+        };
+      },
+    }),
 
-})
+    getListOfPokemon: builder.query({
+      query: () => ({
+        url: 'pokemon',
+        params: {
+          limit: 25,
+        },
+      }),
+    }),
+  }),
 
-export const {useGetPokemonByNameQuery} = pokemonApi
+});
+
+export const { useGetPokemonByNameQuery, useGetListOfPokemonQuery } = pokemonApi;
