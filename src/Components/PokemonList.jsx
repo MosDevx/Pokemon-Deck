7f7f08d91603f58a6612d9ce0750222d41c8cd59
MosDevx <mosesk.wamae@gmail.com>
2023-01-16 08:44:00 +0300

    style filters

--- src/Components/PokemonList.jsx
+++ src/Components/PokemonList.jsx
@@ -12,16 +12,16 @@ const PokemonList = () => {
 
   return (
     <div>
-      <div className="flex w-4/6 m-auto justify-around h-10 my-2">
-        <button className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white " onClick={() => changeType('All')}>All</button>
-        <button  className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('water')}>Water</button>
-        <button className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "   onClick={() => changeType('fire')}>Fire</button>
-        <button  className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('normal')}>Normal</button>
-        <button  className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('electric')}>Electric</button>
-        <button  className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('grass')}>Grass</button>
-        <button  className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('bug')}>Bug</button>
-        <button className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "   onClick={() => changeType('poison')}>Poison</button>
-        <button className="border-2 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "   onClick={() => changeType('ground')}>Ground</button>
+      <div className="flex flex-wrap w-4/6 m-auto justify-around h-32 sm:h-20 md:h-10 my-2">
+        <button className="border-2 h-10 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white " onClick={() => changeType('All')}>All</button>
+        <button  className="border-2  h-10 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('water')}>Water</button>
+        <button className="border-2 h-10  px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "   onClick={() => changeType('fire')}>Fire</button>
+        <button  className="border-2 h-10  px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('normal')}>Normal</button>
+        <button  className="border-2  h-10 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('electric')}>Electric</button>
+        <button  className="border-2  h-10 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('grass')}>Grass</button>
+        <button  className="border-2 h-10 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "  onClick={() => changeType('bug')}>Bug</button>
+        <button className="border-2 h-10 px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "   onClick={() => changeType('poison')}>Poison</button>
+        <button className="border-2 h-10  px-2 py-0 rounded-md font-semibold border-slate-300 hover:bg-sky-400 hover:text-white "   onClick={() => changeType('ground')}>Ground</button>
 
       </div>
 
